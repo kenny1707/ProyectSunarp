24babc7ec15aaf6898eab2190b2e05b709b6a1a4
Kenny Carmona <kennyharold02@gmail.com>
2024-11-12 23:36:01 -0500

    Se sube el projecto de las Ciudades

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
+import { SharedModule } from './shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,7 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/countries/pages/by-country-page/by-country-page.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Country } from '../../interfaces/country';
+import { CountriesService } from '../../services/countries.service';
+
+@Component({
+  selector: 'app-by-country-page',
+  templateUrl: './by-country-page.component.html',
+  styles: ``
+})
+export class ByCountryPageComponent implements OnInit {
+
+  public countries: Country[] = [];
+  public initialValue: string = '';
+
+  constructor(private countriesService: CountriesService) {}
+
+  ngOnInit(): void {
+    this.countries = this.countriesService.cacheStore.byCountries.countries;
+    this.initialValue = this.countriesService.cacheStore.byCountries.term;
+  }
+
+  searchByCountry( country: string):void {
+    this.countriesService.searchCountry( country )
+      .subscribe( countries => {
+        this.countries = countries
+      })
+  }
+
+}
